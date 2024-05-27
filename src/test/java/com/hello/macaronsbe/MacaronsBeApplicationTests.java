c0c9e5e08aba6ac46605ad55ca1e0243218debdb
mgolebiewski <marek@vhive.ai>
2024-05-27 08:57:57 +0200

    First commit

---
+++ src/test/java/com/hello/macaronsbe/MacaronsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.macaronsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaronsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
